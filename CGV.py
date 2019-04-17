61d08121b23f2dae7d322d4e5673550de744b843
DylanMsK <kms920612@gmail.com>
2019-04-17 22:20:56 +0900

    fake useragent

--- CGV.py
+++ CGV.py
@@ -22,24 +22,37 @@ class Mail:
         smtp.sendmail(email, to, msg.as_string())
         smtp.quit()
 
+
 url = 'http://www.cgv.co.kr/theaters/special/theater-line-up.aspx?regioncode=07'
-headers = {
-    'User-Agent': "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
-    }
+user_agent = [
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:64.0) Gecko/20100101 Firefox/64.0",
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15",
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; rv:11.0) like Gecko",
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0",
+    ]
+
 cnt = 0
 while 1:
-    html = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(html, 'html.parser')
-    ul = soup.find('div', class_='sect-lineup').find('sect-chart').find('ul')
-    lis = ul.findAll('li')
-    if len(lis) > 1 or '요로나' in ul.text or cnt == 3:
-        Mail('ㄲㄲㄲㄲ', '?????')
-        break
-    if '요로나' in ul.text:
-        print('요로나')
-        break
-    if cnt == 3:
-        print('cnt')
-        break
+    random_idx = random.choice(range(len(user_agent)))
+    headers = {'User-Agent': user_agent[random_idx]}
+    try:
+        html = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(html, 'html.parser')
+        ul = soup.find('div', class_='sect-lineup').find('ul')
+        lis = ul.findAll('li')
+        if len(lis) > 1 or '엔드게임' in str(ul):
+            Mail('예매!!!!!!!!!!!!', '')
+            break
+        if cnt == 3:
+            print('cnt')
+            break
+    except:
+        user_agent.pop(random_idx)
+        if len(user_agent) == 0:
+            break
     cnt += 1
-    time.sleep(3)
+    time.sleep(random.randint(5, 10))
